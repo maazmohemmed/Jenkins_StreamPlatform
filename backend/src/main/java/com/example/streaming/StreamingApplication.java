3aedb9f214ffaa78f776518f9e185111219488f7
maazmohemmed <maazashrar478@gmail.com>
2025-07-19 20:01:53 +0530

    Update StreamingApplication.java

--- backend/src/main/java/com/example/streaming/StreamingApplication.java
+++ backend/src/main/java/com/example/streaming/StreamingApplication.java
@@ -1,10 +1,21 @@
 package com.example.streaming;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StreamingApplication {
-  public static void main(String[] args) {
-    SpringApplication.run(StreamingApplication.class, args);
-  }
+public class StreamingApplication extends SpringBootServletInitializer {
+
+    // For deploying to external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StreamingApplication.class);
+    }
+
+    // For running locally (e.g., `mvn spring-boot:run`)
+    public static void main(String[] args) {
+        SpringApplication.run(StreamingApplication.class, args);
+    }
 }
